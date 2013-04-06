70cc73d85826de992100a0b836e5fc47dab8de3a
gg <gg@Molly.(none)>
2013-04-06 14:54:33 +0200

    init a git to track changes

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RPWeb::Application.config.secret_token = '99fbaba960d499d5468c6de80c144a977f92ff51f415bcb47398973cf964867d807fdca7bca67e00b75c63c6846b97ca058a5f1a983d5d0ecede2452ad6bdabc'
